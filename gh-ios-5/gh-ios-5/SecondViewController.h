73c632ae89b9a9f501ab2d016c9362326f21d813
clans <vjqcgfv@gmail.com>
2014-11-16 17:03:17 +0200

    Added second table view programmatically.

--- gh-ios-5/gh-ios-5/SecondViewController.h
+++ gh-ios-5/gh-ios-5/SecondViewController.h
@@ -8,8 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SecondViewController : UIViewController
+@interface SecondViewController : UIViewController <UITableViewDataSource>
 
+@property (nonatomic, strong) UITableView *mTableView;
 
 @end
 
--- gh-ios-5/gh-ios-5/SecondViewController.m
+++ gh-ios-5/gh-ios-5/SecondViewController.m
@@ -14,14 +14,38 @@
 
 @implementation SecondViewController
 
+@synthesize mTableView;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    CGFloat height = self.view.frame.size.height - self.tabBarController.tabBar.frame.size.height;
+    CGRect rect = CGRectMake(0, 20, self.view.frame.size.width, height);
+    
+    mTableView = [[UITableView alloc] initWithFrame:rect];
+    [self.view addSubview:mTableView];
+    [mTableView setDataSource:self];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    }
+    
+    [cell.textLabel setText:[[NSString alloc] initWithFormat:@"Item %li", (long)indexPath.row]];
+    
+    return cell;
 }
 
 @end
